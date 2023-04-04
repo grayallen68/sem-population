e2385d0c681ed74e2a139aff6485ade54584159f
grayallen68 <sketchtothreed@gmail.com>
2023-04-04 08:58:16 -0600

    implemented population getting functions

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -81,25 +81,15 @@ public class App
             if (rset.next())
             {
                 System.out.println(rset.getInt("population") + " " + rset.getString("name"));
-//                Employee emp = new Employee();
-//                emp.emp_no = rset.getInt("emp_no");
-//                emp.first_name = rset.getString("first_name");
-//                emp.last_name = rset.getString("last_name");
-//                emp.title = rset.getString("title");
-//                emp.salary = rset.getInt("salary");
-//                emp.dept_name = rset.getString("dept_name");
-//                emp.manager = rset.getString("manager");
 
-//                return emp;
             }
-//            else
-//                return null;
+
         }
         catch (Exception e)
         {
             System.out.println(e.getMessage());
-            System.out.println("Failed to get employee details");
-//            return null;
+            System.out.println("Failed to get population details");
+
         }
     }
 
@@ -119,19 +109,24 @@ public class App
                     "SELECT * FROM country WHERE country.name = '" + name + "' ";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
-            // Return new employee if valid.
             // Check one is returned
             if (rset.next())
             {
 
                 Country country = new Country();
+                String countryCode = rset.getString("country.code");
                 String countryName = rset.getString("country.name");
                 String continent = rset.getString("country.continent");
+                String region = rset.getString("country.region");
                 int population = rset.getInt("country.population");
+                int capitalID = rset.getInt("country.capital");
 
+                country.setCode(countryCode);
                 country.setName(countryName);
                 country.setContinent(continent);
+                country.setRegion(region);
                 country.setPopulation(population);
+                country.setCapital(capitalID);
 
                 return country;
             }
@@ -141,7 +136,85 @@ public class App
         catch (Exception e)
         {
             System.out.println(e.getMessage());
-            System.out.println("Failed to get employee details");
+            System.out.println("Failed to get population details");
+            return null;
+        }
+    }
+
+    public City getCityByID(int cityID){
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+
+            String strSelect =
+                    "SELECT * FROM city WHERE city.id = '" + cityID + "' ";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Check one is returned
+            if (rset.next())
+            {
+                City city = new City();
+                int id = rset.getInt("city.id");
+                String name = rset.getString("city.name");
+                String countryCode = rset.getString("city.countrycode");
+                String district = rset.getString("city.district");
+                long population = rset.getLong("city.population");
+
+                city.setID(id);
+                city.setName(name);
+                city.setCountryCode(countryCode);
+                city.setDistrict(district);
+                city.setPopulation(population);
+
+                return city;
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get population details");
+            return null;
+        }
+    }
+
+    public City getCityByName(String cityName){
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+
+            String strSelect =
+                    "SELECT * FROM city WHERE city.name = '" + cityName + "' ";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Check one is returned
+            if (rset.next())
+            {
+                City city = new City();
+                int id = rset.getInt("city.id");
+                String name = rset.getString("city.name");
+                String countryCode = rset.getString("city.countrycode");
+                String district = rset.getString("city.district");
+                long population = rset.getLong("city.population");
+
+                city.setID(id);
+                city.setName(name);
+                city.setCountryCode(countryCode);
+                city.setDistrict(district);
+                city.setPopulation(population);
+
+                return city;
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get population details");
             return null;
         }
     }
@@ -151,17 +224,34 @@ public class App
         if (country != null)
         {
             System.out.println(
-                    "name: " + country.getName() + "\n"
-            //need to call get continent by id function here
-                    + "continent: " + country.getContinent() + "\n"
-                    + "population: " + country.getPopulation()
+                    "code: " + country.getCode() + "\n" +
+                    "name: " + country.getName() + "\n" +
+            //need to call get city by id function here
+                    "continent: " + country.getContinent() + "\n" +
+                    "region: " + country.getRegion() + "\n" +
+                    "population: " + country.getPopulation() + "\n" +
+                    "capital: " + country.getCapital() + "\n"
+            );
+        }
+    }
+    public void displayCity(City city)
+    {
+        if (city != null)
+        {
+            System.out.println(
+                    "ID: " + city.getID() + "\n" +
+                            "name: " + city.getName() + "\n" +
+                            //need to call get city by id function here
+                            "country code: " + city.getCountryCode() + "\n" +
+                            "district: " + city.getDistrict()+ "\n" +
+                            "population: " + city.getPopulation() + "\n"
             );
         }
     }
 
     //POPULATION INFORMATION REQUESTS
     public long getWorldPopulation(){
-        Long population;
+        long population;
 
         //get population of all countries in the database
         //return the sum of all of them
@@ -188,6 +278,103 @@ public class App
         }
     }
 
+    public long getContinentPopulation(String continent){
+        long population;
+        //get all countries where their continent is equal to the text passed in
+        //sum their populations
+        try{
+            Statement stmt = con.createStatement();
+
+            String strSelect =
+                    "SELECT SUM(population) AS population FROM country "
+                    +"WHERE country.continent = '" + continent + "' ";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            if (rset.next()) {
+                population = rset.getLong("population");
+
+
+                return population;
+            }else{
+                return 0;
+            }
+
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get population details");
+            return 0;
+        }
+    }
+
+    public long getRegionPopulation(String region){
+        long population;
+        //get all countries where their region is equal to the text passed in
+        //sum their populations
+        try{
+            Statement stmt = con.createStatement();
+
+            String strSelect =
+                    "SELECT SUM(population) AS population FROM country "
+                            +"WHERE country.region = '" + region + "' ";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            if (rset.next()) {
+                population = rset.getLong("population");
+
+
+                return population;
+            }else{
+                return 0;
+            }
+
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get population details");
+            return 0;
+        }
+    }
+
+    public long getCountryPopulation(String country){
+        long population;
+        //use getCountryByName function
+        Country c = getCountryByName(country);
+        //print the population and return the
+        return c.getPopulation();
+    }
+
+    public long getDistrictPopulation(String district){
+        long population;
+        //get all cities where district is equal to the text passed in
+        //sum their populations
+        try{
+            Statement stmt = con.createStatement();
+
+            String strSelect =
+                    "SELECT SUM(population) AS population FROM city "
+                            +"WHERE city.district = '" + district + "' ";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            if (rset.next()) {
+                population = rset.getLong("population");
+
+
+                return population;
+            }else{
+                return 0;
+            }
+
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get population details");
+            return 0;
+        }
+    }
+
+    public long getCityPopulation(String cityName){
+        //use get city by name function
+        City city = getCityByName(cityName);
+        return city.getPopulation();
+    }
 
 
 
@@ -203,15 +390,25 @@ public class App
             a.connect(args[0], Integer.parseInt(args[1]));
         }
 
-
         //Test getting country by name
         Country country = a.getCountryByName("Aruba");
         //Test displaying contents of country object
         long p = a.getWorldPopulation();
         System.out.println("World Population: " + p);
 
+        long c = a.getCountryPopulation("Belize");
+        System.out.println("Population: " + c);
 
+        Country tCountry = a.getCountryByName("Belize");
+        a.displayCountry(tCountry);
+
+        City tCity = a.getCityByID(tCountry.getCapital());
+        a.displayCity(tCity);
+
+        long cp = a.getCityPopulation("Belmopan");
+        System.out.println("City Population: " + cp);
         // Disconnect from database
         a.disconnect();
     }
 }
+
--- src/main/java/com/napier/sem/City.java
+++ src/main/java/com/napier/sem/City.java
@@ -1,13 +1,53 @@
 package com.napier.sem;
 
 public class City {
+    //class to represent records in city table of world database
+    //table: | id | name | countryCode | District | Population |
 
     private int ID;
     private String name;
     private String countryCode; //foreign key from country table
     private String district;
-    private int population;
+    private long population;
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public void setPopulation(long population) {
+        this.population = population;
+    }
 
 
+    public int getID() {
+        return ID;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public long getPopulation() {
+        return population;
+    }
 }
